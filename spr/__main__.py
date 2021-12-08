81b68c589d4312179340012e9314c360ec11cc95
insidejob <84038103+insidescode@users.noreply.github.com>
2021-12-08 22:40:37 +0530

    Update __main__.py

--- spr/__main__.py
+++ spr/__main__.py
@@ -49,14 +49,14 @@ async def help_command(_, message: Message):
     kb = ikb(
         {
             "Help": "bot_commands",
-            "Repo": "https://github.com/TheHamkerCat/SpamProtectionRobot",
+            "Repo": "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
             "Add Me To Your Group": f"https://t.me/{BOT_USERNAME}?startgroup=new",
-            "Support Chat (for now)": "https://t.me/WBBSupport",
+            "Support Chat (for now)": "https://t.me/sluttyoreo",
         }
     )
     mention = message.from_user.mention
     await message.reply_photo(
-        "https://hamker.me/logo_3.png",
+        "https://telegra.ph/file/a02ef5602aecde9d58b5a.jpg",
         caption=f"Hi {mention}, I'm SpamProtectionRobot,"
         + " Choose An Option From Below.",
         reply_markup=kb,
